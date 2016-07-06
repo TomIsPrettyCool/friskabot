6313e6ad934672d4e9fa9ecc4de4d14c278ff5ad
Tom Haines <thomas.haines@fromecollege.org>
2016-07-06 16:23:54 +0100

    Initial commit

---
+++ src/friskabot.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+commands = ['/order', '/availiable','/view', '/menu']
+
+
+@app.route('/api/')
+def api():
+    args = request.args
+    command = args.get('command')
+
+    init_command = command.split(' ')[0]
+
+    if init_command in commands:
+        return 'yay'
+    else:
+        return 'arse'
+
+def order():
+    pass
+
+def availiable():
+
